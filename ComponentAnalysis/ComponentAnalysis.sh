f85e77a3f2aab4d772179211338d0b83cb5dbc05
hanjunlee21 <67846757+hanjunlee21@users.noreply.github.com>
2021-08-24 22:18:42 -0400

    Update ComponentAnalysis.sh

--- ComponentAnalysis/ComponentAnalysis.sh
+++ ComponentAnalysis/ComponentAnalysis.sh
@@ -4,7 +4,7 @@ O="GM12878_1vs2"
 PYTHONVERSION="python3.8"
 
 # backup
-cp ~/.local/lib/$PYTHONVERSION/site-packages/skimage/metrics/_structural_similarity.py ~/backup.py
+cp ~/.local/lib/$PYTHONVERSION/site-packages/skimage/metrics/_structural_similarity.py ssim_backup.py
 
 # luminance
 cp ssim_luminance.py ~/.local/lib/$PYTHONVERSION/site-packages/skimage/metrics/_structural_similarity.py
@@ -19,5 +19,5 @@ cp ssim_structure.py ~/.local/lib/$PYTHONVERSION/site-packages/skimage/metrics/_
 chess sim -p 4 ../Processed/$R.chr2.25kb.KR.cool ../Processed/$Q.chr2.25kb.KR.cool ../Pairs/hg19.chr2.2Mb.500kb.txt ../CHESS/chr2.2Mb.500kb/$O.structure.tsv
 
 # recovery
-cp ~/backup.py ~/.local/lib/$PYTHONVERSION/site-packages/skimage/metrics/_structural_similarity.py
+cp ssim_backup.py ~/.local/lib/$PYTHONVERSION/site-packages/skimage/metrics/_structural_similarity.py
 
